6e329f860ba063e3b27dbbeffd6fe0ef67e110f1
wxShawn <1806394760@qq.com>
2019-02-17 16:56:55 +0800

    new page

--- js/index.js
+++ js/index.js
@@ -10,25 +10,84 @@ function addLoadEvent(func){
     }
 }
 
-function setHtmlStyle(){
-    var htmlstyle = document.getElementById('htmlstyle');
-    var width = document.getElementsByTagName('body')[0].offsetWidth;
-    if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent) || width < 800){
-        htmlstyle.setAttribute('href','./css/index-mob.css');
-        // 移动端操作
-    } else {
-        htmlstyle.setAttribute('href','./css/index-pc.css');
-        // PC端操作
+function getScrollTop(){
+    var scrollTop = 0;
+    if(document.documentElement && document.documentElement.scrollTop){
+        scrollTop = document.documentElement.scrollTop;
     }
-    window.onresize = function(){
-        setHtmlStyle();
+    else if(document.body){
+        scrollTop = document.body.scrollTop;
+    }
+    return scrollTop;
+}
+
+function setNavbarStyle(){
+    var nav = document.getElementById('navbar');
+    var nav_w = nav.offsetWidth;
+    var nav_a = nav.getElementsByTagName('a');
+    var a_color = 0;
+    var a_size = 0;
+    var a_margin = 0;
+    var a_width = 0;
+    if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent) || nav_w < 700){
+        a_size = '14px';
+        a_margin = '0';
+        a_width = '90px';
+    }else{
+        a_size = '20px';
+        a_margin = '0 20px';
+        a_width = '120px';
+    }
+    if(getScrollTop() >= 440){
+        nav.className = 'nav-fixed';
+        a_color = '#f9f7f7';
+    }else{
+        nav.className = '';
+        a_color = '#3f72af';
+    }
+    for(var i = 0; i < nav_a.length; i++){
+        nav_a[i].style.color = a_color;
+        nav_a[i].style.fontSize = a_size;
+        nav_a[i].style.margin = a_margin;
+        nav_a[i].style.width = a_width;
     }
 }
 
-addLoadEvent(setHtmlStyle());
+window.onscroll = function(){
+    setNavbarStyle();
+}
+
+window.onresize = function(){
+    setNavbarStyle();
+}
+
+function navbar(){
+    var nav = document.getElementById('navbar');
+    var nav_a = nav.getElementsByTagName('a');
+    var headerH = document.getElementsByTagName('header')[0].offsetHeight;
+    var aboutH = document.getElementById('about').offsetHeight;
+    var porfolioH = document.getElementById('porfolio').offsetHeight;
+    var height = new Array;
+    height[0] = 0;
+    height[1] = headerH - 60;
+    height[2] = height[1] + aboutH;
+    height[3] = height[2] + porfolioH;
+    for(var i = 0; i < nav_a.length; i++){
+        nav_a[i].index = i;
+        nav_a[i].onclick = function(){
+            var j = this.index;
+            window.scrollTo({
+                top: height[j],
+                behavior: 'smooth'
+            })
+        }
+    }
+}
+
+addLoadEvent(navbar());
 
 function setContactIconStyle(color1,color2){
-    var icon = document.getElementsByClassName('icon-link')[0];
+    var icon = document.getElementById('contact-icon');
     var btn = icon.getElementsByTagName('a');
     var elem = icon.getElementsByTagName('path');
     for(var i = 0; i < btn.length; i++){
@@ -48,32 +107,4 @@ function setContactIconStyle(color1,color2){
     }
 }
 
-addLoadEvent(setContactIconStyle('#8a8a8a','#3f72af'));
-
-function modalSwitch(){
-    var body = document.getElementsByTagName('body')[0];
-    var btn = document.getElementsByClassName('switch')[0];
-    var btn_color = btn.getElementsByTagName('path')[0];
-    var modal = document.getElementsByClassName('links')[0];
-    var j = true;
-    btn.onclick = function(){
-        console.log(j);
-        if(j == true){
-            modal.style.animation = 'showModal 1s';
-            modal.style.height = '100%';
-            btn.style.position = 'fixed';
-            btn_color.setAttribute('fill','#3f72af');
-            j = false;
-        }else{
-            modal.style.animation = 'hideModal 1s';
-            modal.style.height = '0';
-            setTimeout(function(){
-                btn_color.setAttribute('fill','#dbe2ef');
-            },800);
-            j = true;
-        }
-        
-    }
-}
-
-addLoadEvent(modalSwitch());
+addLoadEvent(setContactIconStyle('#f9f7f7','#3f72af'));
